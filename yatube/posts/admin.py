6cae8dd7c36ba4660bffadfb9be6d0ff043a3592
Михаил Богачев <bogachevmikhailirkutsk@gmail.com>
2023-01-31 20:36:24 +0800

    commit_2

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -6,7 +6,7 @@ from .models import Post, Group
 class PostAdmin(admin.ModelAdmin):
     """Класс кастомизации модели Post"""
 
-    list_display: tuple[str] = ('pk', 'text', 'pub_date', 'author', 'group')
+    list_display: tuple[str] = ('pk', 'text', 'pub_date', 'author', 'group',)
     list_editable: tuple[str] = ('group',)
     search_fields: tuple[str] = ('text',)
     list_filter: tuple[str] = ('pub_date',)
