d3dff31703dfbc22354cdac2670cead49703b313
Богачев Михаил Викторович <m.bogachev@slata.com>
2023-02-07 17:50:25 +0800

    add paginator

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 
 from .models import Post, Group
 
 
+NUM_POST_FOR_PAGE: int = 10
+
 def index(request):
     """Вью для отображения главной страницы с публикациями"""
     template: str = 'posts/index.html'
-    posts = Post.objects.order_by('-pub_date')[:10]
+    post_list = Post.objects.order_by('-pub_date')
+    paginator = Paginator(post_list, NUM_POST_FOR_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context: dict = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, template, context)
 
@@ -17,9 +23,13 @@ def group_posts(request, slug):
     """Вью для отображения страниц с постами конкретной группы"""
     template: str = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    post_list = Post.objects.filter(group=group).order_by('-pub_date')
+    paginator = Paginator(post_list, NUM_POST_FOR_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context: dict = {
         'group': group,
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, template, context)
