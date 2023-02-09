60f6989f3d3e1cacfde858b0d71f2345c76a6240
Михаил Богачев <bogachevmikhailirkutsk@gmail.com>
2023-02-09 21:08:53 +0800

    pep8

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 
 from .models import Post, Group
+from .forms import PostForm
 
 
 User = get_user_model()
 
 
 NUM_POST_FOR_PAGE: int = 10
+
 
 def index(request):
     """Вью для отображения главной страницы с публикациями"""
@@ -50,7 +53,7 @@ def profile(request, username):
     page_obj = paginator.get_page(page_number)
 
     context = {
-        'user': user,
+        'author': user,
         'page_obj': page_obj,
         'post_count': post_count,
     }
@@ -66,4 +69,39 @@ def post_detail(request, post_id):
         'post': post,
         'post_count': post_count
     }
-    return render(request, template, context) 
+    return render(request, template, context)
+
+
+@login_required
+def post_create(request):
+    form = PostForm(request.POST or None)
+    group = Group.objects.all()
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect(f'/profile/{post.author}/', {
+            'form': form
+        })
+    return render(request, 'posts/create_post.html', {
+        'form': form,
+        'group': group
+    })
+
+
+@login_required
+def post_edit(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    if post.author != request.user:
+        return redirect('posts:post_detail', post_id)
+    form = PostForm(request.POST, instance=post)
+    is_edit = True
+    context = {
+        'form': form,
+        'is_edit': is_edit,
+        'post': post
+    }
+    if form.is_valid():
+        form.save()
+        return redirect('posts:post_detail', post_id)
+    return render(request, 'posts/create_post.html', context)
